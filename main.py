6f801be436b8ce10dd4563722808348c4f41ee5b
evelynod <odonnell.ev@gmail.com>
2025-07-23 19:31:10 -0700

    Add number_of_words function skeleton, Ch2, L4

--- main.py
+++ main.py
@@ -6,22 +6,43 @@
 # 2. Write a main function that uses get_book_text with the relative path to your
 # frankenstein.txt file to print the entire contents of the book to the console
 
+########################################################################################
+
+# Ch 2 L 3 Assignment:
+
+# 1. Write a new function that accepts the text from the book as a string, and returns
+#  the number of words in the string. The .split() method will be helpful here
+# 2. Instead of printing the books contents, print this message to the console:
+
+# "{num_words} words found in the document"
 
 
 
 path_to_file = "books/frankenstein.txt"    
+num_words = 0     # Declare variable for the number of words.
+file_contents = ""
 
 
 def get_book_text(path_to_file): 
     with open(path_to_file) as f:
         file_contents = f.read()   # f is a file object
 
-    print(file_contents)  
+  #  print({file_contents})  
+    return file_contents      #file_contents will be used in number_of_words function
+
+
+def number_of_words(file_contents):
+    #Accept the text from the book as a string, and returns the number of words in the string
+    #Write function here.
+
+
+    print(f"{num_words} words found in the document")
     
 
 
 def main():
     get_book_text(path_to_file)  
+    number_of_words(file_contents) 
     
 
 
