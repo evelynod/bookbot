5d8bef2dfa286fbb31a801d968e84533133c9d12
evelynod <odonnell.ev@gmail.com>
2025-07-23 18:00:51 -0700

    print contents of file as string. Ch 2, L3 Assignment

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("greetings boots")
+# Ch 2 L 3 Assignment: 
+
+# 1. Write a new function called get_book_text.
+# It takes a filepath as input and returns the contents of the file as a string.
+
+# 2. Write a main function that uses get_book_text with the relative path to your
+# frankenstein.txt file to print the entire contents of the book to the console
+
+
+
+
+path_to_file = "books/frankenstein.txt"    
+
+
+def get_book_text(path_to_file): 
+    with open(path_to_file) as f:
+        file_contents = f.read()   # f is a file object
+
+    print(file_contents)  
+    
+
+
+def main():
+    get_book_text(path_to_file)  
+    
+
+
+    
+
+
+
+      
+    
+main()
