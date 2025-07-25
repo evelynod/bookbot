d9b33b3b3e7eed8bd15bf8f82f5af558e73dd96c
evelynod <odonnell.ev@gmail.com>
2025-07-24 22:31:32 -0700

    Cleaned up comments, test print uses smaller word list

--- main.py
+++ main.py
@@ -23,51 +23,42 @@
 
 path_to_file = "books/frankenstein.txt"    
 num_words = 0     # Declare variable for the number of words.
-#file_contents = ""
 split_contents = []
 
 
 def get_book_text(path_to_file): 
     with open(path_to_file) as f:
         file_contents = f.read()   # f is a file object
-
-    #print({file_contents})  
+      
     return file_contents      #file_contents will be used in number_of_words function
 
+
+########## This function will count the words #######
 
 def number_of_words(file_contents) :
     split_contents = file_contents.split()    #split the file into words. At this point it is 
                                               #listing the words, but not counting them.
-
-    return split_contents     
+    
+    return (split_contents)     
 
       
-    
-    
-
+  
 
 def main():
+   
+   
+    my_book_text = get_book_text(path_to_file) 
+    split_text = number_of_words(my_book_text,)   #Call number of words function--still just lists 
+    print(f"Here are the first 20 words in a list: {split_text [:20]}") #This works!!
+
+
+     
     
-   # get_book_text(path_to_file)
-    my_book_text = get_book_text(path_to_file) #Define new variable for get_book_text return value
-    print(f"The first 200 characters: {my_book_text[:200]}")  #test to see if my_book_text works
 
-    ### Now run number_of_words.  The book text was called "file_contents" in the 
-    ### get_book_text function, but it is "my_book_text" in this main function.  
 
-    split_text = number_of_words(my_book_text)   #Call number of words function. 
-    print(f"Here are the first 200 words in a list: {split_text [:200]}") #This works!!
+    
 
 
    
-    
-    
-
-
-    
-
-
-
-      
-    
+ 
 main()
